f2c697ab627b4f6d9e4b56ab2908e055e8e985e2
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2024-02-24 20:41:24 -0500

    especificacion de la base de datos de mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RequestLogSchema } from './modules/request-log/request-log.shema';
 import { RequestLogService } from './modules/request-log/request-log.service';
 import { PostsModule } from './posts/posts.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:example@localhost:27017'), 
-    MongooseModule.forFeature([
-      { name: 'RequestLog', schema: RequestLogSchema },
-    ]),
+    MongooseModule.forRoot('mongodb://root:example@172.17.208.1:27017/mydatabase'), 
     PostsModule,
   ],
-  providers: [RequestLogService],
+  
+  providers: [AppService],
+  controllers:[AppController]
+  
 })
 export class AppModule {}
